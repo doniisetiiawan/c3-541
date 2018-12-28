05d9b4dd1b272c374fbd8dd7f74e1a28b999f97a
Doni Setiawan <donisetiawan.050@gmail.com>
2018-12-28 15:12:42 +0700

    Add chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import c3 from "c3";
+import "./Chart.css";
+
+class Chart extends Component {
+  componentDidMount() {
+    this.updateChart();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.updateChart();
+  }
+
+  render() {
+    return <div id="chart" />;
+  }
+
+  updateChart() {
+    c3.generate({
+      bindto: "#chart",
+      data: { columns: this.props.columns, type: this.props.chartType }
+    });
+  }
+}
+
+export default Chart;
